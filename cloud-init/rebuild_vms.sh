07186922577e03589efcc1bee8459469e0815f26
Chuck Schweizer <csschwe@gmail.com>
2020-04-01 15:57:54 -0500

    initial commit

---
+++ cloud-init/rebuild_vms.sh
@@ -0,0 +1,36 @@
+INSTANCE_HOSTNAME=vminstance-1
+INSTANCE_IMAGE=${INSTANCE_HOSTNAME}.qcow2
+INSTANCE_IMAGE_PATH=/var/lib/libvirt/images/${INSTANCE_HOSTNAME}
+BACKING_FILE=/var/lib/libvirt/images/base/ubuntu-18.04-server-cloudimg-amd64-cusomized.qcow2
+CLOUD_INIT_FILE=${INSTANCE_HOSTNAME}-cidata.iso
+NETWORK_FILE=network-config-${INSTANCE_HOSTNAME}
+
+sudo virsh destroy ${INSTANCE_HOSTNAME}
+sudo virsh undefine ${INSTANCE_HOSTNAME}
+sudo rm -f ${INSTANCE_IMAGE_PATH}/${INSTANCE_IMAGE} ${INSTANCE_IMAGE_PATH}/${CLOUD_INIT_FILE}
+
+sudo mkdir -p ${INSTANCE_IMAGE_PATH}
+
+cloud-localds --verbose --hostname=${INSTANCE_HOSTNAME} --network-config=${NETWORK_FILE} ${CLOUD_INIT_FILE} user-data
+
+sudo cp ${CLOUD_INIT_FILE} ${INSTANCE_IMAGE_PATH}
+
+sudo qemu-img create \
+    -f qcow2 \
+    -o backing_file=${BACKING_FILE} ${INSTANCE_IMAGE_PATH}/${INSTANCE_IMAGE}
+
+sudo qemu-img resize ${INSTANCE_IMAGE_PATH}/${INSTANCE_IMAGE} 10G
+
+sudo virt-install \
+    --connect qemu:///system \
+    --virt-type kvm \
+    --name ${INSTANCE_HOSTNAME} \
+    --ram 1024 \
+    --vcpus=1 \
+    --network bridge=br0 \
+    --os-type linux \
+    --os-variant ubuntu18.04 \
+    --disk path=${INSTANCE_IMAGE_PATH}/${INSTANCE_IMAGE},format=qcow2 \
+    --disk ${INSTANCE_IMAGE_PATH}/${CLOUD_INIT_FILE},device=cdrom \
+    --import \
+    --noautoconsole
---
+++ readme.md
@@ -0,0 +1,48 @@
+# Setup KVM on Ubuntu
+
+https://medium.com/@artem.v.vasilyev/use-ubuntu-cloud-image-with-kvm-1f28c19f82f8 <br>
+https://discourse.ubuntu.com/t/building-multipass-images-with-packer/12361 <br>
+https://docs.ansible.com/ansible/latest/modules/virt_module.html <br>
+https://cloudinit.readthedocs.io/en/latest/index.html <br>
+
+
+```
+sudo apt-get install cloud-utils qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils qemu-utils genisoimage virtinst
+```
+
+```
+# /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    enp7s0:
+      dhcp4: no
+      dhcp6: no
+  bridges:
+    br0:
+      interfaces: [enp7s0]
+      dhcp4: no
+      addresses: [192.168.1.50/24]
+      gateway4: 192.168.1.1
+      nameservers:
+        search: [home]
+        addresses: [192.168.1.1, 8.8.8.8]
+```
+
+# Get packer and build custom image
+```
+wget https://releases.hashicorp.com/packer/1.5.5/packer_1.5.5_linux_amd64.zip
+unzip packer_1.5.5_linux_amd64.zip
+sudo ./packer build ubuntu_build.json
+
+sudo mkdir -p /var/lib/libvirt/images/base
+
+sudo mv output-qemu/packer-qemu /var/lib/libvirt/images/base/ubuntu-18.04-server-cloudimg-amd64-cusomized.qcow2
+```
+
+# Build VMs
+```
+cd cloud-init
+rebuild_vms.sh
+```
